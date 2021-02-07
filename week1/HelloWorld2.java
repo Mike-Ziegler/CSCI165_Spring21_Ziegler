52582aa4659b43bb79142bc98d3044c14dca131d
Mike-Ziegler <mziegler45@gmail.com>
2021-02-06 23:12:55 -0500

    Week1 lab containing HelloWorld, HelloWorld2, Shapes, and text doc with screen shoots

---
+++ week1/HelloWorld2.java
@@ -0,0 +1,7 @@
+public class HelloWorld2 {
+
+    public static void main(String[] args) {
+
+    System.out.println(System.getProperty("java.class.path") + System.getProperty("java.home") + System.getProperty("java.version") + System.getProperty("os.arch") + System.getProperty("os.version") + System.getProperty("user.dir") + System.getProperty("user.home") + System.getProperty("user.name"));
+}
+}
