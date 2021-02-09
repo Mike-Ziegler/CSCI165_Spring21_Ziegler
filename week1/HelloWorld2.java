3346bf6ed69c79853bd6917c9fa7fec2d453c526
Mike-Ziegler <mziegler45@gmail.com>
2021-02-08 19:01:09 -0500

    added command line task to HelloWorld2 program

--- week1/HelloWorld2.java
+++ week1/HelloWorld2.java
@@ -1,6 +1,13 @@
 public class HelloWorld2 {
 
     public static void main(String[] args) {
+        String s = "";
+    for (String t : args)
+        s += t + " ";
+    
+        
+
+        System.out.printf("Hello %s. Nice work processing the arguments\n", s);
 
     System.out.println(System.getProperty("java.class.path") + System.getProperty("java.home") + System.getProperty("java.version") + System.getProperty("os.arch") + System.getProperty("os.version") + System.getProperty("user.dir") + System.getProperty("user.home") + System.getProperty("user.name"));
 }
